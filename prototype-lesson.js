656587ab80c4b9dd761cacf2d0651d0c68975ab6
Mahesh Dathrika <mdathrika@ebay.com>
2015-07-09 00:16:56 -0700

    Update prototype-lesson.js

--- prototype-lesson.js
+++ prototype-lesson.js
@@ -6,14 +6,15 @@ function A()
 }
  
 A.prototype.inAProto = 3;
- 
-B.prototype = new A;            // Hook up A into B's prototype chain
-B.prototype.constructor = B;
+
 function B()
 {
     this.inB = 4;
 }
- 
+
+B.prototype = new A;            // Hook up A into B's prototype chain
+B.prototype.constructor = B;
+
 B.prototype.inBProto = 5;
  
 x = new B;
