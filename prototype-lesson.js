56ffafc3f01839d735fd736b13c0df81e19484f4
Mahesh Dathrika <mdathrika@ebay.com>
2015-07-09 00:15:13 -0700

    Initial commit

---
+++ prototype-lesson.js
@@ -0,0 +1,20 @@
+Object.prototype.inObj = 1;
+ 
+function A()
+{
+    this.inA = 2;
+}
+ 
+A.prototype.inAProto = 3;
+ 
+B.prototype = new A;            // Hook up A into B's prototype chain
+B.prototype.constructor = B;
+function B()
+{
+    this.inB = 4;
+}
+ 
+B.prototype.inBProto = 5;
+ 
+x = new B;
+console.log(x.inObj + ', ' + x.inA + ', ' + x.inAProto + ', ' + x.inB + ', ' + x.inBProto);
